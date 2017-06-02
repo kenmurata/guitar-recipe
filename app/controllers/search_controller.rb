8c9d100b757fada6476348da48197d2a028edc3b
Ken Murata <k_murata@nttpc.co.jp>
2017-06-02 00:20:57 +0000

    Search jissou

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,32 @@ class SearchController < ApplicationController
     def index
     end
     def result
-      #@users = User.find_by(params)
-      @users = User.find_by(name: "#{params[:name]}", email: "#{params[:email]}")
+      sql = create_query(params)
+      @users = User.where(sql)
+      #@users = User.where('name = ? or email = ?', params[:name], params[:email])
     end
+    
+    private
+      def create_query(params)
+        sql = []
+        key = []
+        query = ""
+        count = 0
+        params.each do |param|
+          if param[0] == "utf8" || param[0] == "commit" || param[0] == "controller" || param[0] == "action"
+          else
+            if count != 0
+              query = query + "or "
+            end
+            query = query + "#{param[0]} like ? "
+            count += 1
+            key.push "%#{param[1]}%"
+          end
+        end
+        sql.push query
+        key.each do |k|
+          sql.push k
+        end
+        return sql
+      end
 end
