b54ac3dc85e7b0b3ee2e571aec32fc9dea1fb8c0
Ken Murata <k_murata@nttpc.co.jp>
2018-04-05 14:53:19 +0000

    basic auth kesu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  http_basic_authenticate_with name: "zacodesign", password: "Zaco@00330033"
+  #http_basic_authenticate_with name: "zacodesign", password: "Zaco@00330033"
   
 end
